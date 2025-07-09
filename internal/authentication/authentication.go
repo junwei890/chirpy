6796422276b8174c48102f4eec292b56894bae92
jdub4asdfg <junweilee72@gmail.com>
2025-07-10 00:01:47 +0800

    added creation and validation of jwt tokens, included tests

--- internal/authentication/authentication.go
+++ internal/authentication/authentication.go
@@ -1,7 +1,10 @@
 package authentication
 
 import (
+	"time"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -9,15 +12,46 @@ func HashPassword(password string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	hashedPassword := string(hashedPasswordInBytes)
-	return hashedPassword, nil
+	return string(hashedPasswordInBytes), nil
 }
 
 func CheckPasswordHash(password, hash string) error {
-	passwordInBytes := []byte(password)
-	hashInBytes := []byte(hash)
-	if err := bcrypt.CompareHashAndPassword(passwordInBytes, hashInBytes); err != nil {
+	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil {
 		return err
 	}
 	return nil
+}
+
+func MakeJWT(userID uuid.UUID, secretKey string, expiresIn time.Duration) (string, error) {
+	claims := jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		Subject: userID.String(),
+		IssuedAt: jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+	}
+	createdToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := createdToken.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, secretKey string) (uuid.UUID, error) {
+	claims := &jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
+		return []byte(secretKey), nil
+	})
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	stringifiedUUID, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	returnUUID ,err := uuid.Parse(stringifiedUUID)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return returnUUID, nil
 }
---
+++ tests/authentication_test.go
@@ -0,0 +1,94 @@
+package tests
+
+import (
+	"testing"
+	"time"
+	"github.com/google/uuid"
+	"github.com/junwei890/chirpy/internal/authentication"
+)
+
+func TestHashPassword(t *testing.T) {
+	password1 := "helloworld"
+	password2 := "blazinglyfast"
+	hashedPassword1, _ := authentication.HashPassword(password1)
+	hashedPassword2, _ := authentication.HashPassword(password2)
+
+	testCases := []struct {
+		name string
+		password string
+		hash string
+		errorPresent bool
+	}{
+		{
+			name: "Hash match",
+			password: password1,
+			hash: hashedPassword1,
+			errorPresent: false,
+		},
+		{
+			name: "Hash mismatch",
+			password: password1,
+			hash: hashedPassword2,
+			errorPresent: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := authentication.CheckPasswordHash(testCase.password, testCase.hash)
+			if (err != nil) != testCase.errorPresent {
+				t.Errorf("Test case: %s, failed.", testCase.name)
+			}
+		})
+	}
+}
+
+func TestMakingAndValidatingJWT(t *testing.T) {
+	userID1 := uuid.New()
+	userID2 := uuid.New()
+	secretKey1 := "helloworld"
+	secretKey2 := "blazinglyfast"
+	duration1 := time.Duration(60000000000)
+	duration2 := time.Duration(60)
+	jwtTokenUserID1, _ := authentication.MakeJWT(userID1, secretKey1, duration1)
+	jwtTokenUserID2, _ := authentication.MakeJWT(userID2, secretKey2, duration2)
+
+	testCases := []struct {
+		name string
+		userID uuid.UUID
+		jwtToken string
+		secretKey string
+		errorPresent bool
+	}{
+		{
+			name: "Token is valid",
+			userID: userID1,
+			jwtToken: jwtTokenUserID1,
+			secretKey: secretKey1,
+			errorPresent: false,
+		},
+		{
+			name: "Token is not valid",
+			userID: userID1,
+			jwtToken: jwtTokenUserID1,
+			secretKey: secretKey2,
+			errorPresent: true,
+		},
+		{
+			name: "Token has expired",
+			userID: userID2,
+			jwtToken: jwtTokenUserID2,
+			secretKey: secretKey2,
+			errorPresent: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			_, err := authentication.ValidateJWT(testCase.jwtToken, testCase.secretKey)
+			if (err != nil) != testCase.errorPresent {
+				t.Errorf("Test case: %s, failed.", testCase.name)
+			}
+		})
+	}
+}
